e71d58aa94b6cc7c8c604588eb019f470e9ce694
Flor <fcflorencia@hotmail.es>
2021-02-13 00:38:34 +0000

    gulp-eslint config

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-}
-
-exports.default = defaultTask
+//const {src, task} = require('gulp');
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+ 
+gulp.task('eslint', () => {
+    return gulp.src(['./src/js/*.js'])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
+});
